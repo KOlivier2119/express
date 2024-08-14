7b7f78805dafe3500485825f63e5bc0b45d16d7a
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2024-08-14 12:03:29 +0200

    Defining Cookies

--- routes/products.mjs
+++ routes/products.mjs
@@ -1,8 +1,16 @@
 import { Router } from "express";
-const router = Router()
+const router = Router();
 
-router.get('/api/products', (req, res) => {
-    res.send('Products')
-})
+router.get("/api/products", (req, res) => {
+  console.log(req.headers.cookies);
+  console.log(req.cookies);
+  console.log(req.signedCookies.hello)
 
-export default router
+  if (req.signedCookies.hello && req.signedCookies.hello === "World") return res.send("Products");
+
+  return res.send({
+    msg: "Sorry you need a correct cookie",
+  });
+});
+
+export default router;
--- server.mjs
+++ server.mjs
@@ -1,13 +1,21 @@
 import express from "express";
-
-const app = express();
+import cookieParser from "cookie-parser";
 import { createMovieValidationSchema } from "./utils/validationShemas.mjs";
 import movieRouter from "./routes/movies.mjs";
 import { movies } from "./utils/constants.mjs";
-import productRouter from './routes/products.mjs'
+import productRouter from "./routes/products.mjs";
 
+const app = express();
+
+app.use(express.json())
+app.use(cookieParser('helloworld'))
 app.use(movieRouter);
-app.use(productRouter)
+app.use(productRouter);
+
+app.get("/", (req, res) => {
+  res.cookie('hello', 'World', { maxAge: 12000 * 120 * 60, signed: true})
+  res.status(201).send({ msg: "Hello" });
+});
 
 const PORT = process.env.PORT || 3000;
 
